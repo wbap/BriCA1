15fb4fa4dcfc040790939fd6da697af4bf35d233
ktnyt <kotone@sfc.keio.ac.jp>
2015-09-21 14:34:14 +0900

    Create new documentation branch

--- python/examples/chainer/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ ! -e data.py ]; then
-    wget https://raw.githubusercontent.com/pfnet/chainer/master/examples/mnist/data.py
-fi
-python chainer_sda.py $@
-python validate_sda.py $@
--- python/examples/neuraltalk/run.sh
+++
@@ -1,6 +0,0 @@
-##!/bin/sh
-git clone https://github.com/karpathy/NeuralTalk.git
-mv NeuralTalk/imagernn .
-wget http://cs.stanford.edu/people/karpathy/neuraltalk/flickr8k_cnn_lstm_v1.zip
-unzip flickr8k_cnn_lstm_v1.zip
-python trained_neuraltalk.py NeuralTalk/example_images/vgg_feats.mat NeuralTalk/example_images/tasks.txt flickr8k_cnn_lstm_v1.p
