8edd916882fc4de7ff284bd5321cea06dd4c9ce9
ktnyt <kotone@sfc.keio.ac.jp>
2016-03-09 17:04:06 +0900

    Add supervisor to default modules

--- python/brica1/__init__.py
+++ python/brica1/__init__.py
@@ -10,12 +10,13 @@ A scalable, general purpose computing platform for cognitive architectures.
 
 """
 
-__all__ = ["component", "connection", "module", "port", "ros", "scheduler", "unit", "utils"]
+__all__ = ["component", "connection", "module", "port", "ros", "scheduler", "supervisor", "unit", "utils"]
 
 from .component import *
 from .connection import *
 from .module import *
 from .port import *
 from .scheduler import *
+from .supervisor import *
 from .unit import *
 from .utils import *
