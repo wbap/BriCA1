490d7509c37562fd8fbf9d934050f944e09f1ffd
ARAKAWA Naoya <naoya.arakawa@nifty.com>
2021-02-18 17:18:10 +0900

    setup.py NAME BriCA1 > brica1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'BriCA1'
+NAME = 'brica1'
 DESCRIPTION = 'Brain-inspired Computing Architecture Version 1'
 URL = 'https://github.com/wbap/BriCA1'
 EMAIL = 'kotone@sfc.keio.ac.jp'
