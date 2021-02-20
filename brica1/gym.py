bbfe62a253727a5924f8df0ace905a4a20752951
ARAKAWA Naoya <naoya.arakawa@nifty.com>
2021-02-20 17:44:09 +0900

    Introducing done and info attributes to EnvComponent

--- brica1/gym.py
+++ brica1/gym.py
@@ -28,26 +28,25 @@ class EnvComponent(Component):
         self.make_in_port('action', 1)
         self.make_out_port('observation', 1)
         self.make_out_port('reward', 1)
-        self.make_out_port('done', 1)
-        self.make_out_port('info', 1)
+        self.done = False
+        self.info = None
 
         observation = env.reset()
 
         self.get_out_port('observation').buffer = observation
         self.results['observation'] = observation
-        self.get_out_port('done').buffer = False
-        self.results['done'] = False
 
     def fire(self):
         action = self.inputs['action'][0]
         observation, reward, done, info = self.env.step(action)
+        self.info = info
         if done:
             self.env.reset()
+            self.done = True
+        else:
+            self.done = False
         self.results['observation'] = observation
         self.results['reward'] = np.array([reward])
-        # self.results['done'] = done
-        # self.results['info'] = info
-
 
 class GymAgent(Agent):
     def __init__(self, model, env):
@@ -63,18 +62,12 @@ class GymAgent(Agent):
 
         self.make_in_port('observation', 1)
         self.make_in_port('reward', 1)
-        self.make_in_port('done', 1)
-        self.make_in_port('info', 1)
         self.make_out_port('action', 1)
 
         alias_in_port((model, 'observation'), (self, 'observation'))
-        alias_in_port((model, 'reward'), (self, 'reward'))
-        alias_in_port((model, 'done'), (self, 'done'))
-        alias_in_port((model, 'info'), (self, 'info'))
         alias_out_port((model, 'action'), (self, 'action'))
+        alias_in_port((model, 'reward'), (self, 'reward'))
 
         connect((self.env, 'observation'), (self, 'observation'))
         connect((self.env, 'reward'), (self, 'reward'))
-        connect((self.env, 'done'), (self, 'done'))
-        connect((self.env, 'info'), (self, 'info'))
         connect((self, 'action'), (self.env, 'action'))
