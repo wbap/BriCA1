f80649309fb180e30d666cfa10824253451e8114
ARAKAWA Naoya <naoya.arakawa@nifty.com>
2021-04-20 17:13:23 +0900

    added token synchronization code to gym.py

--- brica1/gym.py
+++ brica1/gym.py
@@ -26,27 +26,47 @@ class EnvComponent(Component):
         self.env = env
 
         self.make_in_port('action', 1)
+        self.make_in_port('token_in', 1)
         self.make_out_port('observation', 1)
         self.make_out_port('reward', 1)
-        self.done = False
+        self.make_out_port('done', 1)
+        self.make_out_port('token_out', 1)
+        self.done = 0
         self.info = None
+        self.cnt = 0
+        self.flush = False
 
         observation = env.reset()
-
-        self.get_out_port('observation').buffer = observation
         self.results['observation'] = observation
+        self.results['token_out'] = np.array([self.cnt])
 
     def fire(self):
-        action = self.inputs['action'][0]
-        observation, reward, done, info = self.env.step(action)
-        self.info = info
-        if done:
-            self.env.reset()
-            self.done = True
+        if self.flush:
+            return
+        if self.inputs['token_in'][0]==self.cnt:
+            action = self.inputs['action'][0]
+            observation, reward, done, info = self.env.step(action)
+            self.info = info
+            self.cnt += 1
+            # print("Gym:", self.cnt, observation, reward, done, action)
+            self.results['token_in'] = self.inputs['token_in']
+            if done:
+                self.done = 1
+            else:
+                self.done = 0
+            self.results['observation'] = observation
+            self.results['reward'] = np.array([reward])
+            self.results['done'] = np.array([self.done])
+            self.results['token_out'] = np.array([self.cnt])
         else:
-            self.done = False
-        self.results['observation'] = observation
-        self.results['reward'] = np.array([reward])
+             self.results['token_out'] = self.inputs['token_in']
+
+    def reset(self):
+        self.cnt = 0
+        self.flush = False
+        self.results['observation'] = self.env.reset()
+        self.results['token_out'] = np.array([self.cnt])
+        self.inputs['token_in'] = np.array([self.cnt])
 
 class GymAgent(Agent):
     def __init__(self, model, env):
@@ -62,12 +82,21 @@ class GymAgent(Agent):
 
         self.make_in_port('observation', 1)
         self.make_in_port('reward', 1)
+        self.make_in_port('done', 1)
+        self.make_in_port('token_in', 1)
         self.make_out_port('action', 1)
+        self.make_out_port('token_out', 1)
 
         alias_in_port((model, 'observation'), (self, 'observation'))
-        alias_out_port((model, 'action'), (self, 'action'))
         alias_in_port((model, 'reward'), (self, 'reward'))
+        alias_in_port((model, 'done'), (self, 'done'))
+        alias_in_port((model, 'token_in'), (self, 'token_in'))
+        alias_out_port((model, 'action'), (self, 'action'))
+        alias_out_port((model, 'token_out'), (self, 'token_out'))
 
         connect((self.env, 'observation'), (self, 'observation'))
         connect((self.env, 'reward'), (self, 'reward'))
+        connect((self.env, 'done'), (self, 'done'))
+        connect((self.env, 'token_out'), (self, 'token_in'))
         connect((self, 'action'), (self.env, 'action'))
+        connect((self, 'token_out'), (self.env, 'token_in'))
