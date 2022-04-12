2e9fb36736d6403aad0ff0a2b9ce093e6ad3bb18
ARAKAWA Naoya <naoya.arakawa@nifty.com>
2022-04-12 17:29:55 +0900

    Changed EnvComponent.fire() & Component.__init__()

--- brica1/brica_gym.py
+++ brica1/brica_gym.py
@@ -12,14 +12,12 @@ to the respective `Port`s inside the `GymAgent`.
 
 import numpy as np
 
-from brica1 import Component
-from brica1 import Module, Agent
-from brica1 import alias_in_port, alias_out_port, connect
+import brica1
 
 __all__ = ['EnvComponent', 'GymAgent']
 
 
-class EnvComponent(Component):
+class EnvComponent(brica1.Component):
     def __init__(self, env):
         super(EnvComponent, self).__init__()
 
@@ -45,52 +43,47 @@ class EnvComponent(Component):
     def fire(self):
         if self.flush:
             return
-        if self.cnt == 0:   # after reset
-            self.cnt = 1
-            self.results['token_out'] = np.array([self.cnt])
-        elif self.inputs['token_in'][0] == self.cnt:
-            if self.inputs['action'].size == 1:
-                action = self.inputs['action'][0]
-            else:   # one-hot vector to int
-                if self.inputs['action'].max() == 0.0:
-                    action = 0
+        if self.inputs['token_in'][0] == self.cnt:
+            if self.cnt != 0:
+                if self.inputs['action'].size == 1:
+                    action = self.inputs['action'][0]
+                else:   # one-hot vector to int
+                    if self.inputs['action'].max() == 0.0:
+                        action = 0
+                    else:
+                        action = np.argmax(self.inputs['action']) + 1
+                observation, reward, done, info = self.env.step(action)
+                self.info = info
+                self.results['observation'] = observation
+                self.results['reward'] = np.array([reward])
+                if done:
+                    self.done = 1
                 else:
-                    action = np.argmax(self.inputs['action']) + 1
-            observation, reward, done, info = self.env.step(action)
-            self.info = info
+                    self.done = 0
+                self.results['done'] = np.array([self.done])
             self.cnt += 1
-            # print("Gym:", self.cnt, observation, reward, done, action)
-            self.results['token_in'] = self.inputs['token_in']
-            if done:
-                self.done = 1
-            else:
-                self.done = 0
-            self.results['observation'] = observation
-            self.results['reward'] = np.array([reward])
-            self.results['done'] = np.array([self.done])
-            self.results['token_out'] = np.array([self.cnt])
-        else:
-             self.results['token_out'] = self.inputs['token_in']
+        self.results['token_out'] = np.array([self.cnt])
 
     def reset(self):
         self.cnt = 0
         self.flush = False
+        self.done = False
         self.results['observation'] = self.env.reset()
         self.results['done'] = np.array([0])
         self.results['reward'] = np.array([0.0])
         self.results['token_out'] = np.array([0])
         self.inputs['token_in'] = np.array([0])
 
-class GymAgent(Agent):
+class GymAgent(brica1.Agent):
     def __init__(self, model, env):
         super(GymAgent, self).__init__()
 
         self.env = EnvComponent(env)
         self.add_component('env', self.env)
 
-        if isinstance(model, Component):
+        if isinstance(model, brica1.Component):
             self.add_component('model', model)
-        if isinstance(model, Module):
+        if isinstance(model, brica1.Module):
             self.add_submodule('model', model)
 
         self.make_in_port('observation', 1)
@@ -100,16 +93,46 @@ class GymAgent(Agent):
         self.make_out_port('action', 1)
         self.make_out_port('token_out', 1)
 
-        alias_in_port((model, 'observation'), (self, 'observation'))
-        alias_in_port((model, 'reward'), (self, 'reward'))
-        alias_in_port((model, 'done'), (self, 'done'))
-        alias_in_port((model, 'token_in'), (self, 'token_in'))
-        alias_out_port((model, 'action'), (self, 'action'))
-        alias_out_port((model, 'token_out'), (self, 'token_out'))
+        brica1.utils.alias_in_port((model, 'observation'), (self, 'observation'))
+        brica1.utils.alias_in_port((model, 'reward'), (self, 'reward'))
+        brica1.utils.alias_in_port((model, 'done'), (self, 'done'))
+        brica1.utils.alias_in_port((model, 'token_in'), (self, 'token_in'))
+        brica1.utils.alias_out_port((model, 'action'), (self, 'action'))
+        brica1.utils.alias_out_port((model, 'token_out'), (self, 'token_out'))
 
-        connect((self.env, 'observation'), (self, 'observation'))
-        connect((self.env, 'reward'), (self, 'reward'))
-        connect((self.env, 'done'), (self, 'done'))
-        connect((self.env, 'token_out'), (self, 'token_in'))
-        connect((self, 'action'), (self.env, 'action'))
-        connect((self, 'token_out'), (self.env, 'token_in'))
+        brica1.utils.connect((self.env, 'observation'), (self, 'observation'))
+        brica1.utils.connect((self.env, 'reward'), (self, 'reward'))
+        brica1.utils.connect((self.env, 'done'), (self, 'done'))
+        brica1.utils.connect((self.env, 'token_out'), (self, 'token_in'))
+        brica1.utils.connect((self, 'action'), (self.env, 'action'))
+        brica1.utils.connect((self, 'token_out'), (self.env, 'token_in'))
+
+
+class ComponentCaller(type):
+    def __new__(mcs, name, base, attr):
+        # suppress Component.fire()
+        if name != "Component":
+            # fire() gets decorated.
+            attr["fire"] = mcs.decorate(attr["fire"])
+        return super().__new__(mcs, name, base, attr)
+
+    @classmethod
+    def decorate(mcs, fire):
+        def new_fire(self):
+            if self.inputs['token_in'][0] == self.token + 1:
+                self.token = self.inputs['token_in'][0]
+                fire(self)
+            self.results['token_out'] = self.inputs['token_in']
+        return new_fire
+
+
+class Component(brica1.Component, metaclass=ComponentCaller):
+    def __init__(self):
+        super(Component, self).__init__()
+        self.token = 0
+        self.make_in_port('token_in', 1)
+        self.make_out_port('token_out', 1)
+
+    def fire(self):
+        pass
+
